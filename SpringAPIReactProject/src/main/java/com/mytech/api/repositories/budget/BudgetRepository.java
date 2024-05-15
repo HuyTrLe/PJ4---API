b0952e27dd9e8ac8e54096ec4d12c65b63979428
tranhoangtu007@gmail.com <tranhoangtu007@gmail.com>
2024-05-15 17:00:15 +0700

    update budget

--- SpringAPIReactProject/src/main/java/com/mytech/api/repositories/budget/BudgetRepository.java
+++ SpringAPIReactProject/src/main/java/com/mytech/api/repositories/budget/BudgetRepository.java
@@ -47,7 +47,7 @@ public interface BudgetRepository extends JpaRepository<Budget, Integer> {
 			@Param("periodStart") LocalDate periodStart, @Param("periodEnd") LocalDate periodEnd,
 			@Param("budgetId") Integer budgetId);
 	
-	@Query("SELECT new com.mytech.api.models.budget.BudgetResponse(t.budgetId,t.amount, t.threshold_amount, c.name, ci.path,t.periodStart,t.periodEnd) FROM Budget t JOIN t.category c JOIN c.icon ci WHERE t.user.id = :userId and t.periodStart < :fromDate ")
+	@Query("SELECT new com.mytech.api.models.budget.BudgetResponse(t.budgetId,t.amount, t.threshold_amount, c.name, ci.path,t.periodStart,t.periodEnd) FROM Budget t JOIN t.category c JOIN c.icon ci WHERE t.user.id = :userId and t.periodEnd < :fromDate ")
 	List<BudgetResponse> getBudgetPast(int userId, LocalDate fromDate);
 	
 	@Query("SELECT new com.mytech.api.models.budget.BudgetResponse(t.budgetId,t.amount, t.threshold_amount, c.name, ci.path,t.periodStart,t.periodEnd) FROM Budget t JOIN t.category c JOIN c.icon ci WHERE t.user.id = :userId and t.periodStart > :fromDate ")
